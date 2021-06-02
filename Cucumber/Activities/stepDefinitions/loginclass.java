1a176f7d2bf6e99c65e7f37ec29f83ccc8d565a5
Sejalkumari03 <84365880+Sejalkumari03@users.noreply.github.com>
2021-06-02 10:27:23 +0530

    Add files via upload

---
+++ Cucumber/Activities/stepDefinitions/loginclass.java
@@ -0,0 +1,30 @@
+package stepDefinitions;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.When;
+
+
+public class loginclass {
+	
+	@Given("^user is on the Amazon website$")
+	
+	public void user_is_on_Home_Page() {
+	WebDriver driver= new FirefoxDriver();
+	driver.get("http://www.amazon.in");
+	
+	
+	//@When("^user searches a product$") 	
+	
+		//public static void user() throws Throwable {
+		//driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+		
+//}
+	}
+		
+	}
+	
+
---
+++ Cucumber/Activities/testRunner/testrunner.java
@@ -0,0 +1,22 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"pretty"},
+    
+    //plugin = {"html: test-reports"},
+    //plugin = {"json: test-reports/json-report.json"},
+    monochrome = true
+)
+
+public class testrunner {
+    //empty
+}
+
